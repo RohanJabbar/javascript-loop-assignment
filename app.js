4124857d4e5bde01645aff8dd20eed86de407c30
RohanJabbar <muhammadrohan714@gmail.com>
2023-10-22 14:22:12 +0500

    loop assignment

---
+++ app.js
@@ -0,0 +1,82 @@
+// Question no 1
+// for
+//    (var i = 2; i<= 50; i+=2){
+//      console.log(i)
+//    }
+
+// Question no 2
+
+// for
+// (var i = 1; i<= 50; i+=2){
+//     console.log(i)
+// }
+
+
+
+
+// // Question no 03
+
+
+//     for (var i = 1; i <= 50; i++) {
+    
+//         if (i % 2 === 0) {
+//         console.log("Even: " + i);
+//     } else {
+//         console.log("Odd: " + i);
+//     }
+// }
+
+// Question no 4
+
+// var num = 10
+
+
+// for (var i = 1; i <= num; i++){
+//     var Rows = " ";
+
+//     for ( var j = 1 ; j <= i ; j++ ){
+//         Rows +=j;
+//     }
+// console.log(Rows);
+// }
+// Question no 5
+
+// var students = [
+//     { Name: "Rohan", 
+//     Age: 23, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Ammad",
+//      Age: 25,
+//      Class: "WEB DEVELOPMENT" },
+//     { Name: "Zesshan", 
+//     Age: 24, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "shehryar", 
+//     Age: 23, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "A.Rehman", 
+//     Age: 26, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Ahsan", 
+//     Age: 25, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Saqib", 
+//     Age: 27, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Yousuf", 
+//     Age: 21, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Ameer Hamza", 
+//     Age: 21, 
+//     Class: "WEB DEVELOPMENT" },
+//     { Name: "Kawish", 
+//     Age: 21, 
+//     Class: "WEB DEVELOPMENT" },
+   
+// ]
+// for (var i = 0; i < students.length; i++) {
+    
+//     console.log("Name:" + students[i].Name)
+//     console.log("Age:" + students[i].Age)
+//     console.log("Class:" + students[i].Class)
+// }
